76bc32f9b0544e54035d000650d39e477a02089b
Shayan Khan <shayanzahoornsr@gmail.com>
2024-08-02 12:48:01 -0700

    Commit untracked and modified files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wondertales/Views/CreateStory.dart';
+import 'package:wondertales/Views/Our%20Story/Play_Story.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Createstory(),
+      home: Play_Story(),
     );
   }
 }
